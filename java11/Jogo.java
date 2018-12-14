8cd078cedac3667e22bd47bc4c0eed88f5ecfc65
Marco Mangan <marco.mangan@gmail.com>
2018-12-14 16:57:23 -0200

    #1

--- java11/Jogo.java
+++ java11/Jogo.java
@@ -1,7 +1,7 @@
 import java.awt.*;
 
 
-public class Jogo {
+public class Jogo extends Panel {
 
 public static void main(String args[]) throws Exception {
 GraphicsEnvironment ge = GraphicsEnvironment.
@@ -14,7 +14,12 @@ Frame f = new Frame(gc);
       f.setLocation(10 + bounds.x, 10 + bounds.y);
 	f.setTitle("Oh, hello!");
 	f.setSize(800,600);
+	f.add(new Jogo());
 	f.setVisible(true);
 	Thread.sleep(100);
+}
+	public void paint(Graphics g) {
+		Graphics2D g2 = (Graphics2D)g;
+		g2.fillRect(10, 10, 2, 2);
 }
 }
